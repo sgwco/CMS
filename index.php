35d8dad99611b1a91480fe275436e9b6c989c86c
Vo Hoai Son <vector.mic@gmail.com>
2018-03-19 20:54:01 +0700

    Connected to GraphQL library

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use \GraphQL\Error\Debug;
+use \GraphQL\Type\Definition\Type;
+use \GraphQL\Type\Definition\ObjectType;
+use \GraphQL\Type\Schema;
 use \GraphQL\GraphQL;
 use \SaiGonWeb\Config;
 use \SaiGonWeb\Core\Connection;
@@ -13,16 +15,59 @@ else {
   Config::create_config_database();
 }
 
-// if (!empty($_GET['debug'])) {
-//   set_error_handler(function($severity, $message, $file, $line) use (&$phpErrors) {
-//     throw new ErrorException($message, 0, $severity, $file, $line);
-//   });
-//   $debug = Debug::INCLUDE_DEBUG_MESSAGE || Debug::INCLUDE_TRACE;
-// }
+try {
+  $queryType = new ObjectType([
+    'name' => 'Query',
+    'fields' => [
+      'echo' => [
+        'type' => Type::string(),
+        'args' => [
+          'message' => ['type' => Type::string()]
+        ],
+        'resolve' => function ($root, $args) {
+          return $root['prefix'] . $args['message'];
+        }
+      ]
+    ]
+  ]);
 
-// try {
-//   $result = GraphQL::executeQuery();
-// }
-// catch (\Exception $error) {
+  $mutationType = new ObjectType([
+    'name' => 'Calc',
+    'fields' => [
+      'sum' => [
+        'type' => Type::int(),
+        'args' => [
+          'x' => ['type' => Type::int()],
+          'y' => ['type' => Type::int()]
+        ],
+        'resolve' => function ($root, $args) {
+          return $args['x'] + $args['y'];
+        }
+      ]
+    ]
+  ]);
 
-// }
+  $schema = new Schema([
+    'query' => $queryType,
+    'mutation' => $mutationType
+  ]);
+
+  $rawInput = file_get_contents('php://input');
+  $input = json_decode($rawInput, true);
+  $query = $input['query'];
+  $variableValues = isset($input['variables']) ? $input['variables'] : null;
+
+  $rootValue = ['prefix' => 'You said: '];
+  $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+  $output = $result->toArray();
+}
+catch (\Exception $e) {
+  $output = [
+    'error' => [
+      'message' => $e->getMessage()
+    ]
+    ];
+}
+
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($output);
