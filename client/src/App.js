772052cbe78add8730ff9b92a71ca9fae725c984
Vo Hoai Son <vector.mic@gmail.com>
2018-03-20 02:21:27 +0700

    Modify server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Route exact path='/admin' component={Admin} />
+          <Route exact path='/about' component={About} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+const Admin = () => (
+  <div>
+    <h2>Admin</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -5,17 +5,31 @@ import bodyParser from 'body-parser';
 
 import { schema } from './schemas';
 
-const PORT = 4000;
+const PORT = 8000;
 const server = express();
+
+let publicPath = null;
+let indexPath = null;
+
+if (process.env.NODE_ENV === 'production') {
+  publicPath = path.resolve(__dirname, '..', '..', 'client', 'build');
+  indexPath = path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html');
+}
+else {
+  publicPath = path.resolve(__dirname, '..', 'client', 'build');
+  indexPath = path.resolve(__dirname, '..', 'client', 'build', 'index.html');
+}
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-server.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+
+
+server.use('/public', express.static(publicPath));
 server.get('*', (req, res, next) => {
-  console.log(req.url)
-  if (req.url === '/graphql' || req.url === '/graphiql') return next();
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(indexPath);
 });
+
+
 
 server.listen(PORT, () => console.log(`GraphQL server is now running on http://localhost:${PORT}`));
